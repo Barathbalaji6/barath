9be78a8dba7e7939ba53d601137c222afda72292
Barathbalaji6 <barathbalaji6@gmail.com>
2017-06-21 15:51:23 +0530

    Update longest.java

--- longest.java
+++ longest.java
@@ -1,17 +1,17 @@
 public String longestCommonPrefix(String[] strs) {
-    String longestPrefix = "";
+    String longestPre = "";
     if(strs.length>0){
-        longestPrefix = strs[0];
+        longestPre = strs[0];
     }
     for(int i=1; i<strs.length; i++){
         String analyzing = strs[i];
         int j=0;
-        for(; j<Math.min(longestPrefix.length(), strs[i].length()); j++){
-            if(longestPrefix.charAt(j) != analyzing.charAt(j)){
+        for(; j<Math.min(longestPre.length(), strs[i].length()); j++){
+            if(longestPre.charAt(j) != analyzing.charAt(j)){
                 break;
             }
         }
-        longestPrefix = strs[i].substring(0, j);
+        longestPre = strs[i].substring(0, j);
     }
-    return longestPrefix;
+    return longestPre;
 }
