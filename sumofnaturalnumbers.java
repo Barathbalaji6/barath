4843c9c308af97edfc6b72e8f8fbd16941ef3033
Barathbalaji6 <barathbalaji6@gmail.com>
2017-06-23 09:40:30 +0530

    Create sumofnaturalnumbers.java

---
+++ sumofnaturalnumbers.java
@@ -0,0 +1,17 @@
+public class Natural 
+{
+    public static void main(String args[])
+    {
+        int n, i = 1 ;
+        int sum = 0;
+        System.out.println("Enter Number of items :");
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        while(i <= n)
+        {
+            sum = sum +i;
+            i++;
+        }
+        System.out.println("Sum of "+n+" numbers is :"+sum);
+    } 
+}
