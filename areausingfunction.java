01df5b18e200413bd69b568283935e175ecbd080
Barathbalaji6 <barathbalaji6@gmail.com>
2017-07-11 14:43:26 +0530

    Create areausingfunction.java

---
+++ areausingfunction.java
@@ -0,0 +1,36 @@
+package aa;
+
+import java.util.Scanner;
+
+public class Area {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		Scanner scan = new Scanner(System.in);
+		float r=5f;
+		float length=15;
+		float breadth=10;
+		float height=6;
+		
+		System.out.println("Area of Circle : "+circle(r));
+		System.out.println("Area of Rectangle : "+rec(length,breadth));
+		System.out.println("Area of Triangle : "+tri(breadth,height));
+	}
+
+	private static float tri(float breadth, float height) {
+		
+		return ((breadth*height)/2);
+	}
+
+	private static float rec(float length, float breadth) {
+		
+		return length*breadth;
+		
+	}
+
+	private static float circle(float r) {
+		
+		return (float)3.14*r*r;
+	}
+	
+}
