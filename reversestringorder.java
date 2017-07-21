746787531619a781d597b3d62d77b081e1379514
Barathbalaji6 <barathbalaji6@gmail.com>
2017-07-21 10:05:47 +0530

    Create reversestringorder.java

---
+++ reversestringorder.java
@@ -0,0 +1,14 @@
+package aa;
+
+import java.util.*;
+@SuppressWarnings("unused")
+public class ReverseStringOrder {
+
+	
+	public static void main(String[] args) {
+  StringBuffer st=new StringBuffer("Barath"); 
+  st.reverse();
+  System.out.println(st);
+	}
+
+}
